9c3ee3d589367d035586b6bacf5a46dc056fc711
Allistair Vilakazi <allistair.vilakazi@gmail.com>
2025-02-17 22:58:55 +0200

    chore: debugging

--- backend/mt5/app/routes/order.py
+++ backend/mt5/app/routes/order.py
@@ -79,6 +79,10 @@ def send_market_order_endpoint():
             'BUY': mt5.ORDER_TYPE_BUY,
             'SELL': mt5.ORDER_TYPE_SELL
         }
+        order_type_filling_dict = {
+            'ORDER_FILLING_IOC': mt5.mt5.ORDER_FILLING_IOC,
+            'ORDER_FILLING_FOK': mt5.mt5.ORDER_FILLING_FOK
+        }
 
         order_type_str = data['type'].upper()  # Ensure case-insensitivity
 
@@ -120,6 +124,7 @@ def send_market_order_endpoint():
         # Send order
         result = mt5.order_send(request_data)
         if result is None:  # Handle failed order submission
+            logger.error(f"Error in send_market_order: {request_data} - {mt5.ORDER_FILLING_FOK} - {mt5.ORDER_FILLING_IOC")
             error_code, error_str = mt5.last_error()
             return jsonify({
                 "error": "Order failed",
