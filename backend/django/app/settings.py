9161cd6861fcc9e3e0b595083f0c67ca687b9006
Allistair Vilakazi <allistair.vilakazi@gmail.com>
2025-03-02 20:39:46 +0200

    fix: remove comments

--- backend/django/app/settings.py
+++ backend/django/app/settings.py
@@ -208,13 +208,5 @@ CELERY_BEAT_SCHEDULE = {
     'run-quant-entry-algorithm': {
         'task': 'quant.tasks.run_quant_entry_algorithm',  # This should match the @shared_task name
         'schedule': 60.0 * 1,
-    }
-    # 'run-quant-trailing-stop-algorithm': {
-    #     'task': 'quant.tasks.run_quant_trailing_stop_algorithm',  # This should match the @shared_task name
-    #     'schedule': 15,
-    # },
-    # 'run-quant-close-algorithm': {
-    #     'task': 'quant.tasks.run_quant_close_algorithm',  # This should match the @shared_task name
-    #     'schedule': 15,
-    # },
+    },
 }
